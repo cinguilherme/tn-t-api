1491bf1089d62282cc32eb63fd03f3729a8414c4
Guilherme Cintra <cinguilherme@gmail.com>
2023-06-19 08:10:11 -0300

    logical deletion impl

--- src/db/operationsQueries.ts
+++ src/db/operationsQueries.ts
@@ -1,73 +1,92 @@
 // src/db/operationsQueries.ts
-import { DynamoDB } from "aws-sdk";
-import { randomUUID } from "crypto";
-import { Operation } from "../models/Operation";
+import {DynamoDB} from "aws-sdk";
+import {randomUUID} from "crypto";
+import {Operation} from "../models/Operation";
 
 import dynamoDb from "./dynamoDbClient";
 
 export const createOperation = async (
-  operation: Operation
+    operation: Operation
 ): Promise<Operation> => {
-  operation.id = randomUUID();
-  const params: DynamoDB.DocumentClient.PutItemInput = {
-    TableName: "Operations",
-    Item: operation,
-  };
+    const item = {
+        id: randomUUID(),
+        ...operation,
+        status: "ACTIVE",
+    }
+    const params: DynamoDB.DocumentClient.PutItemInput = {
+        TableName: "Operations",
+        Item: item,
+    };
 
-  await dynamoDb.put(params).promise();
+    await dynamoDb.put(params).promise();
 
-  return operation;
+    return operation;
 };
 
 export const getOperationById = async (id: string): Promise<Operation> => {
-  const params = {
-    TableName: "Operations",
-    Key: {
-      id,
-    },
-  };
+    const params = {
+        TableName: "Operations",
+        Key: {
+            id,
+        },
+    };
 
-  try {
-    const result = await dynamoDb.get(params).promise();
-    return result.Item as Operation;
-  } catch (error) {
-    console.error("Error getting operation by ID:", error);
-    throw error;
-  }
+    try {
+        const result = await dynamoDb.get(params).promise();
+        return result.Item as Operation;
+    } catch (error) {
+        console.error("Error getting operation by ID:", error);
+        throw error;
+    }
 };
 
 export const deleteOperationById = async (id: string): Promise<boolean> => {
-  const params = {
-    TableName: "Operations",
-    Key: {
-      id,
-    },
-  };
+    const params = {
+        TableName: "Operations",
+        Key: {
+            id,
+        },
+        UpdateExpression: "set #status = :status",
+        ExpressionAttributeNames: {
+            "#status": "status",
+        },
+        ExpressionAttributeValues: {
+            ":status": "DELETED",
+        },
+        ReturnValues: "UPDATED_NEW",
+    };
 
-  try {
-    const result = await dynamoDb.delete(params).promise();
-    return !!result;
-  } catch (error) {
-    console.error("Error getting operation by ID:", error);
-    throw error;
-  }
+    try {
+        const result = await dynamoDb.update(params).promise();
+        return !!result;
+    } catch (error) {
+        console.error("Error getting operation by ID:", error);
+        throw error;
+    }
 };
 
 export const getAllOperations = async (): Promise<Operation[]> => {
-  try {
-    const params: AWS.DynamoDB.DocumentClient.ScanInput = {
-      TableName: "Operations",
-    };
+    try {
+        const params: AWS.DynamoDB.DocumentClient.ScanInput = {
+            TableName: "Operations",
+            FilterExpression: "#status = :status",
+            ExpressionAttributeNames: {
+                "#status": "status",
+            },
+            ExpressionAttributeValues: {
+                ":status": "ACTIVE",
+            }
+        };
 
-    const response = await dynamoDb.scan(params).promise();
+        const response = await dynamoDb.scan(params).promise();
 
-    if (response.Items) {
-      return response.Items as Operation[];
-    } else {
-      return [];
+        if (response.Items) {
+            return response.Items as Operation[];
+        } else {
+            return [];
+        }
+    } catch (error) {
+        console.error("Error getting all operations:", error.message);
+        throw error;
     }
-  } catch (error) {
-    console.error("Error getting all operations:", error.message);
-    throw error;
-  }
 };
--- src/db/recordsQueries.ts
+++ src/db/recordsQueries.ts
@@ -1,57 +1,70 @@
 // src/db/recordsQueries.ts
-import { DynamoDB } from "aws-sdk";
-import { randomUUID } from "crypto";
-import { Record } from "../models/Record";
+import {DynamoDB} from "aws-sdk";
+import {randomUUID} from "crypto";
+import {Record} from "../models/Record";
 import dynamoDb from "./dynamoDbClient";
 
 export const addRecord = async (record: Record): Promise<Record> => {
-  const item = { id: randomUUID(), ...record };
-  const params: DynamoDB.DocumentClient.PutItemInput = {
-    TableName: "Records",
-    Item: item,
-  };
+    const item = {id: randomUUID(), ...record, status: "ACTIVE"};
+    const params: DynamoDB.DocumentClient.PutItemInput = {
+        TableName: "Records",
+        Item: item,
+    };
 
-  await dynamoDb.put(params).promise();
+    await dynamoDb.put(params).promise();
 
-  return item;
+    return item;
 };
 
 export const getAllRecords = async (): Promise<Record[]> => {
-  const params: DynamoDB.DocumentClient.ScanInput = {
-    TableName: "Records",
-  };
+    const params: DynamoDB.DocumentClient.ScanInput = {
+        TableName: "Records",
+    };
 
-  const result = await dynamoDb.scan(params).promise();
-  return result.Items as Record[];
+    const result = await dynamoDb.scan(params).promise();
+    return result.Items as Record[];
 };
 
 export const getRecordsByUser = async (userId: string, limit: number, lastKey?: string): Promise<Record[]> => {
-  
-  const params: DynamoDB.DocumentClient.QueryInput = {
-    TableName: "Records",
-    IndexName: "userIdIndex",
-    KeyConditionExpression: "user_id = :userId",
-    ExpressionAttributeValues: {
-      ":userId": userId,
-    },
-    ScanIndexForward: false,
-    Limit: limit ? limit : undefined,
-    ExclusiveStartKey: lastKey ? { id: lastKey } : undefined,
-  };
 
-  const result = await dynamoDb.query(params).promise();
-  return result.Items as Record[];
+    const params: DynamoDB.DocumentClient.QueryInput = {
+        TableName: "Records",
+        IndexName: "userIdIndex",
+        KeyConditionExpression: "user_id = :userId",
+        FilterExpression: "#status = :status",
+        ExpressionAttributeNames: {
+            "#status": "status",
+        },
+        ExpressionAttributeValues: {
+            ":userId": userId,
+            ":status": "ACTIVE",
+        },
+        ScanIndexForward: false,
+        Limit: limit ? limit : 1000,
+        ExclusiveStartKey: lastKey ? {id: lastKey} : undefined,
+    };
+
+    const result = await dynamoDb.query(params).promise();
+    return result.Items as Record[];
 };
 
 export const deleteRecordById = async (id: string): Promise<{ id: string }> => {
-  const params: DynamoDB.DocumentClient.DeleteItemInput = {
-    TableName: "Records",
-    Key: {
-      id,
-    },
-  };
+    const params = {
+        TableName: "Records",
+        Key: {
+            id,
+        },
+        UpdateExpression: "set #status = :status",
+        ExpressionAttributeNames: {
+            "#status": "status",
+        },
+        ExpressionAttributeValues: {
+            ":status": "DELETED",
+        },
+        ReturnValues: "UPDATED_NEW",
+    };
 
-  await dynamoDb.delete(params).promise();
+    await dynamoDb.update(params).promise();
 
-  return { id };
+    return {id};
 };
